8757e854afbf5c95707eeba921ad361465d43a84
siczekkonrad <konradd.siczek@gmail.com>
2017-01-08 18:39:17 +0100

    set-props && putpur-component-render

---
+++ output-component-render-method/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello World </h1>
+        <b>Bold</b>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ set-props-on-react-components/src/App.js
@@ -0,0 +1,21 @@
+
+import React, {Component} from 'react';
+
+class App extends React.Component {
+	render() {
+		let txt = this.props.txt
+		return <h1> {txt} </h1>
+	}
+}
+
+App.propTypes = {
+	txt : React.PropTypes.string,
+	cat : React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+	txt : "Setted heading by default prop"
+}
+
+
+export default App;
