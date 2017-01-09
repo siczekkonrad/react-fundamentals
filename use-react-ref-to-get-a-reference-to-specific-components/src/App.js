62867a8e5549524626ba5b53d7003c216f0c1648
siczekkonrad <konradd.siczek@gmail.com>
2017-01-09 01:01:49 +0100

    use-react-ref-to-get-a-reference-to-specific-components

---
+++ use-react-ref-to-get-a-reference-to-specific-components/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {a:'', b: ''}
+  }
+
+  update() {
+    this.setState({
+      a: this.a.refs.input.value,
+      b: this.refs.b.value
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Input ref={component => this.a = component} update={this.update.bind(this)} />
+        {this.state.a}
+        <hr />
+        <input ref="b" type="text" onChange={this.update.bind(this)} />
+        {this.state.b}
+      </div>
+    )
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return <div> <input ref="input" type="text" onChange={this.props.update} /></div>
+  }
+}
+
+export default App;
