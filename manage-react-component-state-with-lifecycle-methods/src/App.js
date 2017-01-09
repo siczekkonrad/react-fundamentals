6ab84661fcee55a96b67aa316d1453ac1f8c4c8e
siczekkonrad <konradd.siczek@gmail.com>
2017-01-09 11:48:41 +0100

    manage-react-component-state-with-lifecycle-methods

---
+++ manage-react-component-state-with-lifecycle-methods/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {val : 0}
+    this.update = this.update.bind(this)
+  }
+
+  update() {
+    this.setState({val : this.state.val + 1 })
+  }
+
+  componentWillMount() {
+    console.log('component Will Mount');
+    this.setState({m : 2})
+  }
+
+  render() {
+    console.log('render');
+    return <button onClick={this.update}> {this.state.val * this.state.m} </button>    
+  }
+
+  componentDidMount() {
+    console.log('component Did Mount');
+    this.inc = setInterval(this.update, 500);
+  }
+
+  componentWillUnmount() {
+    console.log('component Will Unmount');
+    clearInterval(this.inc);
+  }
+}
+
+
+class Wrapper extends React.Component {
+  constructor() {
+    super();
+  }
+
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
+
+  render() {
+    return (
+      <div>
+          <button onClick={this.mount.bind(this)}> Mount </button>
+          <button onClick={this.unmount.bind(this)}> UnMount </button>
+          <div id="a"></div>
+      </div>
+    )
+  }
+}
+
+export default Wrapper;
